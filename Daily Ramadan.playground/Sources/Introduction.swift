4cf298fb795ef93c134e70c2bdac8b8807027daa
Firdaus <daus@wartek.belajar.id>
2021-04-19 14:17:35 +0800

    Remove space

--- Daily Ramadan.playground/Sources/Introduction.swift
+++ Daily Ramadan.playground/Sources/Introduction.swift
@@ -32,7 +32,7 @@ public class Introduction: UIView {
         
         Hope you're safe and doing great for this special month.
 
-         More than 1.5 billion Muslims from around the world doing fasting, in April 2021, through this game I would like to provide information about how the experiences of people who fast in the month of Ramadan.
+        More than 1.5 billion Muslims from around the world doing fasting, in April 2021, through this game I would like to provide information about how the experiences of people who fast in the month of Ramadan.
 
         In this game there will be 3 sessions according to the daily routine taught in Islam, each session the player will play game about choosing what activity is good to do, the better the activity, the more score will be obtained.
         """
